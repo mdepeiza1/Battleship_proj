e4f4f5262b8cfea4efdb457311508215acf703e9
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-01 15:05:18 -0500

    Added Destroyer, AircraftCarrier, Battleship, and Submarine classes.

--- Battleship/Battleship.cs
+++ Battleship/Battleship.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Battleship
 {
-    abstract class Ship
+    class Battleship
     {
     }
 }
--- Battleship/BattleshipBoard.cs
+++ Battleship/BattleshipBoard.cs
@@ -8,6 +8,9 @@ namespace Battleship
 {
     class BattleshipBoard : IBoard
     {
+        public Player player1;
+        public Player player2;
+
         public void CreateBoard()
         {
 
