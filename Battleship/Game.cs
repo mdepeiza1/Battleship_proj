197262652786b8478d443956242a4395d73cd947
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-03 15:07:03 -0500

    Ship placement game flow was added for Player 2 in Game class. Shot game flow needs to be added.

--- Battleship/Game.cs
+++ Battleship/Game.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Battleship
@@ -30,9 +32,9 @@ namespace Battleship
                 Console.WriteLine("4:Aircraft Carrier");
 
 
-                if (Int32.TryParse(Console.ReadLine(), out int chosenShip))
+                if (Int32.TryParse(Console.ReadLine(), out int chosenShip1))
                 {
-                    switch (chosenShip)
+                    switch (chosenShip1)
                     {
                         case 1:
                             Console.WriteLine("Please enter the row of the front of the destroyer:");
@@ -115,16 +117,16 @@ namespace Battleship
                             }
                             break;
                         case 3://battleship logic
-                            Console.WriteLine("Please enter the row of the front of the destroyer:");
+                            Console.WriteLine("Please enter the row of the front of the battleship:");
                             if (Int32.TryParse(Console.ReadLine(), out int value9))
                             {
-                                Console.WriteLine("Please enter the column of the front of the destroyer:");
+                                Console.WriteLine("Please enter the column of the front of the battleship:");
                                 if (Int32.TryParse(Console.ReadLine(), out int value10))
                                 {
-                                    Console.WriteLine("Please enter the row of the back of the destroyer:");
+                                    Console.WriteLine("Please enter the row of the back of the battleship:");
                                     if (Int32.TryParse(Console.ReadLine(), out int value11))
                                     {
-                                        Console.WriteLine("Please enter the column of the back of the destroyer:");
+                                        Console.WriteLine("Please enter the column of the back of the battleship:");
                                         if (Int32.TryParse(Console.ReadLine(), out int value12))
                                         {
                                             board.player1.AddShips(value9, value10, value11, value12, 3);
@@ -203,6 +205,199 @@ namespace Battleship
                
             }
             board.UpdateBoard();
+            Console.WriteLine("Board will be cleared in 8 seconds.");
+            Thread.Sleep(8000);
+            Console.Clear();
+            Console.WriteLine("Please press enter for Player 2 to place their ships:");
+            Console.ReadLine();
+            bool continuelooping2 = true;
+            while (continuelooping2)
+            {
+
+                Console.WriteLine("Please enter the number of the ship to place:");
+                Console.WriteLine("1:Destroyer");
+                Console.WriteLine("2:Submarine");
+                Console.WriteLine("3:Battleship");
+                Console.WriteLine("4:Aircraft Carrier");
+
+
+                if (Int32.TryParse(Console.ReadLine(), out int chosenShip2))
+                {
+                    switch (chosenShip2)
+                    {
+                        case 1:
+                            Console.WriteLine("Please enter the row of the front of the destroyer:");
+                            if (Int32.TryParse(Console.ReadLine(), out int value1))
+                            {
+                                Console.WriteLine("Please enter the column of the front of the destroyer:");
+                                if (Int32.TryParse(Console.ReadLine(), out int value2))
+                                {
+                                    Console.WriteLine("Please enter the row of the back of the destroyer:");
+                                    if (Int32.TryParse(Console.ReadLine(), out int value3))
+                                    {
+                                        Console.WriteLine("Please enter the column of the back of the destroyer:");
+                                        if (Int32.TryParse(Console.ReadLine(), out int value4))
+                                        {
+                                            board.player2.AddShips(value1, value2, value3, value4, 1);
+                                            if (board.player2.fleet.Count == 4)
+                                            {
+                                                continuelooping2 = false;
+                                            }
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Please enter an integer.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Please enter an integer.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter an integer.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please enter an integer.");
+                            }
+                            break; //check that it's only exiting the switch statement
+                        case 2://submarine logic
+                            Console.WriteLine("Please enter the row of the front of the submarine:");
+                            if (Int32.TryParse(Console.ReadLine(), out int value5))
+                            {
+                                Console.WriteLine("Please enter the column of the front of the submarine:");
+                                if (Int32.TryParse(Console.ReadLine(), out int value6))
+                                {
+                                    Console.WriteLine("Please enter the row of the back of the submarine:");
+                                    if (Int32.TryParse(Console.ReadLine(), out int value7))
+                                    {
+                                        Console.WriteLine("Please enter the column of the back of the submarine:");
+                                        if (Int32.TryParse(Console.ReadLine(), out int value8))
+                                        {
+                                            board.player2.AddShips(value5, value6, value7, value8, 2);
+                                            if (board.player2.fleet.Count == 4)
+                                            {
+                                                continuelooping2 = false;
+                                            }
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Please enter an integer.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Please enter an integer.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter an integer.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please enter an integer.");
+                            }
+                            break;
+                        case 3://battleship logic
+                            Console.WriteLine("Please enter the row of the front of the battleship:");
+                            if (Int32.TryParse(Console.ReadLine(), out int value9))
+                            {
+                                Console.WriteLine("Please enter the column of the front of the battleship:");
+                                if (Int32.TryParse(Console.ReadLine(), out int value10))
+                                {
+                                    Console.WriteLine("Please enter the row of the back of the battleship:");
+                                    if (Int32.TryParse(Console.ReadLine(), out int value11))
+                                    {
+                                        Console.WriteLine("Please enter the column of the back of the battleship:");
+                                        if (Int32.TryParse(Console.ReadLine(), out int value12))
+                                        {
+                                            board.player2.AddShips(value9, value10, value11, value12, 3);
+                                            if (board.player2.fleet.Count == 4)
+                                            {
+                                                continuelooping2 = false;
+                                            }
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Please enter an integer.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Please enter an integer.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter an integer.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please enter an integer.");
+                            }
+                            break;
+                        case 4://aircraft carrier logic
+                            Console.WriteLine("Please enter the row of the front of the aircraft carrier:");
+                            if (Int32.TryParse(Console.ReadLine(), out int value13))
+                            {
+                                Console.WriteLine("Please enter the column of the front of the aircraft carrier:");
+                                if (Int32.TryParse(Console.ReadLine(), out int value14))
+                                {
+                                    Console.WriteLine("Please enter the row of the back of the aircraft carrier:");
+                                    if (Int32.TryParse(Console.ReadLine(), out int value15))
+                                    {
+                                        Console.WriteLine("Please enter the column of the back of the aircraft carrier:");
+                                        if (Int32.TryParse(Console.ReadLine(), out int value16))
+                                        {
+                                            board.player2.AddShips(value13, value14, value15, value16, 4);
+                                            if (board.player2.fleet.Count == 4)
+                                            {
+                                                continuelooping2 = false;
+                                            }
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Please enter an integer.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Please enter an integer.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter an integer.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please enter an integer.");
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+
+            }
+            board.UpdateBoard();
+            Console.WriteLine("Board will be cleared in 8 seconds.");
+            Thread.Sleep(8000);
+            Console.Clear();
+            Console.WriteLine("Please press enter to begin the game:");
             Console.ReadLine();
         }
     }
